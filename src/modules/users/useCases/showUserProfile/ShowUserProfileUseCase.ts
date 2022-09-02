db9fa3e8509cbf91b59278a7dea945c654269a51
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-09-02 10:12:50 -0300

    fix: get user route

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const currentUser = this.usersRepository.findById(user_id);
+
+    if (!currentUser) {
+      throw new Error("User does not exist");
+    }
+
+    return currentUser;
   }
 }
 
