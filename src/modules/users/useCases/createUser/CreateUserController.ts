159c4ee6c7d7893b2113733616ddc347b114fa11
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-09-02 10:02:29 -0300

    fix: create user route

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,15 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email } = request.body;
+
+    try {
+      this.createUserUseCase.execute({ name, email });
+    } catch (error) {
+      return response.status(400).json(error);
+    }
+
+    return response.status(201).send();
   }
 }
 
