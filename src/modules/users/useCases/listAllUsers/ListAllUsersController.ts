7cb3c31efe17f7fee75b0e828f3e4c057b47c493
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-09-02 10:19:05 -0300

    fix: list users route

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
@@ -6,7 +7,23 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    if (user_id instanceof Array) {
+      return response
+        .status(400)
+        .json({ error: "Request only accepts one user_id" });
+    }
+
+    let users: User[] = [];
+
+    try {
+      users = this.listAllUsersUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(400).json(error);
+    }
+
+    return response.status(200).json(users);
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const currentRequestingUser = this.usersRepository.findById(user_id);
+
+    // TODO add restriction in case user is not admin
+    // if (!currentRequestingUser.isAdmin) {
+    //   throw new Error("Forbidden request");
+    // }
+
+    return this.usersRepository.list();
   }
 }
 
