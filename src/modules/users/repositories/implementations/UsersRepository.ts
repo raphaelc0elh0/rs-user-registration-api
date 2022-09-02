4215665dd98521d381f31489b0720ee230fcc86b
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-09-02 10:25:37 -0300

    fix: model and repo

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,40 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      admin: false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const currentUser = this.findByEmail(receivedUser.email);
+
+    if (!currentUser) return null;
+    currentUser.admin = true;
+    currentUser.updated_at = new Date();
+    return currentUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
